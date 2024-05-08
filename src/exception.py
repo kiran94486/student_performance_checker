4e8e0753d7a3d34c34003c86e0a7d85267a172cc
kiran94486 <kiran.deep94486@gmail.com>
2024-05-08 21:21:07 +0530

    updated

--- src/exception.py
+++ src/exception.py
@@ -18,9 +18,3 @@ class CustomeException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomeException(e,sys)
